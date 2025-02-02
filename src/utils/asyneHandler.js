d9b43c9cd2954b4e84c19bc49061238d651591ab
Uthoai Marma <uthoaimarma597@gmail.com>
2025-02-02 20:06:59 +0600

    utils - asyneHandler,ApiError,ApiResponse

---
+++ src/utils/asyneHandler.js
@@ -0,0 +1,18 @@
+const asyneHandler = (requestHandler)=>{
+    async(req, res, next)=>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
+    }
+}
+
+export {asyneHandler}
+
+// const asyneHandler = (fnc) => async(req, res, next)=> {
+//     try {
+//         await fnc(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
