b9177d0913f4dd25779a2608f7c821ab5396a057
Uthoai Marma <uthoaimarma597@gmail.com>
2025-03-10 18:55:52 +0600

    fixed user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -191,8 +191,8 @@ const logoutUser = asyneHandler( async(req, res)=>{
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1
             }
         },
         {
